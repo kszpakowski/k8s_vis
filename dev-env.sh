612b362e09a0e09c7eeb031aa66dfc762389d251
Karol Szpakowski <karol.szpakowski@gmail.com>
2021-11-07 18:24:31 +0100

    pods and ns vis

--- dev-env.sh
+++ dev-env.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -v $(pwd):/app -it -p8080:8080 golang "bash"
+docker run --rm -v $(pwd):/app -v $(echo "$HOME/.kube/config"):/root/.kube/config -it -p8080:8080 golang "bash"
--- main.go
+++ main.go
@@ -1,23 +1,69 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"html/template"
+	"log"
 	"net/http"
 	"path"
+	"path/filepath"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
-type Book struct {
-	Title  string
-	Author string
+type Namespace struct {
+	Id   string
+	Name string
+}
+
+type Pod struct {
+	Id        string
+	Name      string
+	Namespace string
+}
+
+type ClusterData struct {
+	Namespaces []Namespace
+	Pods       []Pod
+}
+
+type ClusterVisHandler struct {
+	client *kubernetes.Clientset
 }
 
 func main() {
-	http.HandleFunc("/", ShowBooks)
-	http.ListenAndServe(":8080", nil)
+
+	log.Println("Starting")
+
+	visHandler := &ClusterVisHandler{client: CreateK8sClient()}
+	http.HandleFunc("/", visHandler.RenderCluster)
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		panic(err.Error())
+	}
 }
 
-func ShowBooks(w http.ResponseWriter, r *http.Request) {
-	book := Book{"Building Web Apps with Go", "Jeremy Saenz"}
+func (ClusterVisHandler *ClusterVisHandler) RenderCluster(w http.ResponseWriter, r *http.Request) {
+	cluster := ClusterData{}
+
+	namespaces, err := ClusterVisHandler.client.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for _, ns := range namespaces.Items {
+		cluster.Namespaces = append(cluster.Namespaces, Namespace{"ns_" + ns.Name, ns.Name})
+	}
+
+	pods, err := ClusterVisHandler.client.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+
+	for _, pod := range pods.Items {
+		cluster.Pods = append(cluster.Pods, Pod{"pod_" + pod.Name, pod.Name, pod.Namespace})
+	}
 
 	fp := path.Join("templates", "index.html")
 	tmpl, err := template.ParseFiles(fp)
@@ -26,7 +72,31 @@ func ShowBooks(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err := tmpl.Execute(w, book); err != nil {
+	if err := tmpl.Execute(w, cluster); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func CreateK8sClient() *kubernetes.Clientset {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return client
 }
